5f2335a4232f93658193f94434bff6e96dcce21a
Anton <2004lol222@mail.ru>
2023-12-02 19:45:30 +0800

    modificated file example003_sum, add Random

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numA = 3;
-int numB = 5;
+﻿int numA = new Random().Next(1,10);
+Console.WriteLine(numA + "     это рандомное число из а");
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB + "     это рандомное число из в");
 Console.WriteLine(numA + numB);
