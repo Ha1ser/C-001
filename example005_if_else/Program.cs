87d1ee75bad7c3757a8c26745594c82e91f196e2
Anton <2004lol222@mail.ru>
2023-12-02 20:25:07 +0800

    работа с условием...

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите ваше имя: ");
+String username = Console.ReadLine();
+
+
+if (username.ToLower() == "антон") {
+    Console.Write("Добро пожаловать Господь бог");
+} else {
+    Console.Write("Даааарова ");
+    Console.WriteLine(username);
+}
