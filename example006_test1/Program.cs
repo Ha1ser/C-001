bb284ab4a72a3ad9b66840b84b1c7826a1781351
Anton <2004lol222@mail.ru>
2023-12-02 20:43:04 +0800

    задача с гирями

---
+++ example006_test1/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 20;
+int b = 8;
+int c = 32;
+int d = 24;
+int i = 16;
+
+int max = a;
+
+if (b > max) { max = b;}
+if (c > max) {max = c;}
+if (d > max) {max = d;}
+if (i > max) {max = i;}
+
+Console.WriteLine(max);
+
