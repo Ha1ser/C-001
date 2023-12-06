a6a4e2f4309e4ab20c159223977023dd230906e3
Anton <2004lol222@mail.ru>
2023-12-07 01:19:37 +0800

    еще про масивы

---
+++ exapmple010_array++/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {12, 45, 4, 32, 89, 4, 72};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n){
+    if (array[index] == find) {
+        Console.WriteLine(index + "  каеф");
+        break;
+        }
+    index++;
+
+}
+
+
