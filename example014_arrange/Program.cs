7528c40366cc44cafcf690664dacab7d4bda9309
Anton <2004lol222@mail.ru>
2023-12-19 00:37:43 +0800

    задача 14

---
+++ example014_arrange/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArr(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelSort(int[] array){
+    for(int i = 0; i < array.Length - 1; i++){
+        int minPos = i;
+
+        for(int u = i + 1; u < array.Length; u++){
+            if(array[u] > array[minPos]) {minPos = u;}
+        }
+        int temp = array[i];
+        array[i] =  array[minPos];
+        array[minPos] = temp;
+
+    }
+}
+PrintArr(arr);
+
+SelSort(arr);
+PrintArr(arr);
