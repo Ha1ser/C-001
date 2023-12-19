ac666fce3653d45557c5c9733f6cb2d87882efb6
Anton <2004lol222@mail.ru>
2023-12-19 19:08:18 +0800

    factorial

---
+++ example017_fact_2_array/Program.cs
@@ -0,0 +1,11 @@
+﻿double Fact (int n){
+    // 1! = 1
+
+    if(n == 1) return 1;
+    else return n * Fact(n - 1);
+}
+
+for (int i = 1; i < 40; i++){
+    Console.WriteLine($"{i}! = {Fact(i)}");
+}
+
