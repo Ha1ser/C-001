f94d7b5e85b841760eb60d52d5545a99fbdbf0ed
Anton <2004lol222@mail.ru>
2023-12-19 17:41:41 +0800

    двумерный массив + метод для него

---
+++ example015_array_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// двумерный массив для строк
+// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+
+// for (int rows = 0; rows < 2; rows++){
+//     for(int columns = 0; columns < 5; columns++){
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+
+// }
+
+
+
+
+void PrintArr(int[,] mat){
+    for (int rows = 0; rows < mat.GetLength(0); rows++){
+        for(int columns = 0; columns < mat.GetLength(1); columns++){
+            Console.Write($"{mat[rows, columns]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] mat){
+    for(int i = 0; i < mat.GetLength(0); i++){
+        for(int j = 0; j < mat.GetLength(1); j++){
+            mat[i, j] = new Random().Next(1, 10);
+        }
+    }
+    
+}
+
+int[,] matrix = new int[3, 4];
+PrintArr(matrix);
+
+Console.WriteLine();
+
+FillArray(matrix);
+PrintArr(matrix);
